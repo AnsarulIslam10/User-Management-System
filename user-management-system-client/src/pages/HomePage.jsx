10ae30bd0ed6f370cb57cc36714706223484d950
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-11-30 12:22:17 +0600

    delete from database and ui

--- user-management-system-client/src/pages/HomePage.jsx
+++ user-management-system-client/src/pages/HomePage.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaPen, FaUser, FaX } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const HomePage = () => {
+  const loadedUser = useLoaderData();
+  const [users, setUsers] = useState(loadedUser);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/users/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "User has been deleted.",
+                icon: "success",
+              });
+              const remaining = users.filter((user) => user._id !== id);
+              setUsers(remaining);
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div className="max-w-7xl px-2 mx-auto">
       <div className="mt-16">
@@ -30,21 +64,23 @@ const HomePage = () => {
             </thead>
             <tbody>
               {/* row 1 */}
-              <tr>
-                <th>1</th>
-                <td>Cy Ganderton</td>
-                <td>asr@gmail.com</td>
-                <td>Male</td>
-                <td>Active</td>
-                <td className="space-x-2">
+              {users.map((user, index) => (
+                <tr key={user._id}>
+                  <th>{index + 1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.gender}</td>
+                  <td>{user.status}</td>
+                  <td className="space-x-2">
                     <button className="text-blue-600 p-3 hover:shadow-sm shadow-md hover:bg-base-200">
-                        <FaPen/>
+                      <FaPen />
                     </button>
-                    <button className="text-blue-600 p-3 hover:shadow-sm shadow-md hover:bg-base-200">
-                        <FaX/>
+                    <button onClick={()=> handleDelete(user._id)} className="text-blue-600 p-3 hover:shadow-sm shadow-md hover:bg-base-200">
+                      <FaX />
                     </button>
-                </td>
-              </tr>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
